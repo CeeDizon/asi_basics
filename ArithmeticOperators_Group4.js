b55d7315629b8741a106db84c3b77ea816538c6b
Maycee Dizon <94109584+CeeDizon@users.noreply.github.com>
2021-12-05 19:56:35 +0800

    Add files via upload

---
+++ ArithmeticOperators_Group4.js
@@ -0,0 +1,21 @@
+//CS-401
+// Aquino, Bea Nichole - Dizon, Maycee - Quizon, Megan Rae - Serrano, Christian Harnole
+
+//declared numbers
+let nam1 = 12;
+let nam2 = 2;
+
+console.log("The declared numbers are " + nam1 + " and " + nam2 + "\n");
+
+//arithmetic operators
+let addition = nam1 + nam2;
+let subtraction = nam1-nam2;
+let multiplication = nam1*nam2;
+let division = nam1/nam2;
+
+
+// display of the results
+console.log("The sum of "+nam1+" and "+nam2+" is "+addition);
+console.log("The difference of "+nam1+" and "+nam2+" is "+subtraction);
+console.log("The product of "+nam1+" and "+nam2+" is "+multiplication);
+console.log("The quotient of "+nam1+" and "+nam2+" is "+division);
---
+++ String_Group4.js
@@ -0,0 +1,20 @@
+//CS-401
+// Aquino, Bea Nichole - Dizon, Maycee - Quizon, Megan Rae - Serrano, Christian Harnole
+
+//Declaring strings
+let stringwan = "Hello";
+let stringtwo = "Goodbye";
+
+//conditional statement
+if (stringwan === stringtwo) {
+    console.log("The strings are the same.\n");
+}else{
+    console.log("The strings are not the same.\n")
+}
+
+console.log("String1\t  String2\n");
+
+//loop the input and output the strings 10 times 
+for (let index = 0; index<10; index++) {
+    console.log(stringwan + "\t  " + stringtwo);
+}  
